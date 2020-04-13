edfed2a871def4ce5a6a8e9f46d496e92244186b
veronikahar <veronika.har@hotmail.com>
2020-04-13 15:00:50 -0500

    Update script.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,7 @@ function addImgItem() {
     $imgEl.classList.add('tile');
     $imgEl.setAttribute('src', './css/1.jpg');
     var $boardEl = document.getElementsByClassName('board')[0];
-    console.log('BOard', $boardEl)
     $boardEl.appendChild($imgEl);
-
-    console.log('BOard', $boardEl)
 
     // Event listener that dispalys Thank you modal details upon clicking on image tile
     $imgEl.addEventListener('click', () => {
