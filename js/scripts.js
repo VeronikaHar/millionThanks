11c4c31348da3aca06ab13d219ded48eb82a19f1
veronikahar <veronika.har@hotmail.com>
2020-04-15 17:32:19 -0500

    Update index.html and hover effect.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 //Function that creates 10K 10px*10px image tiles
-function addImgItem() {
+function addTileItem() {
   //Shaping DOM structure
-  for (let i = 0; i < 3000; i++) {
-    var $imgEl = document.createElement('img');
-    $imgEl.classList.add('tile');
+  for (let i = 0; i < 1000; i++) {
+    let $tileEl = document.createElement('div');
+    $tileEl.classList.add('tile');
+    let $imgEl = document.createElement('img');
     $imgEl.setAttribute('src', './css/1.jpg');
-    var $boardEl = document.getElementsByClassName('board')[0];
-    $boardEl.appendChild($imgEl);
+    $tileEl.appendChild($imgEl);
+    let $boardEl = document.getElementsByClassName('board')[0];
+    $boardEl.appendChild($tileEl);
 
     // Event listener that dispalys Thank you modal details upon clicking on image tile
-    $imgEl.addEventListener('click', () => {
+    $tileEl.addEventListener('click', () => {
       // showModal();
     });
   }
 }
 
-addImgItem();
+addTileItem();
